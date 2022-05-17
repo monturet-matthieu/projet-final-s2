a20ddc09d473cfebba53b615490a9bc2dac4dbb5
monturet-matthieu <matthieu.monturet@edu.univ-fcomte.fr>
2022-05-17 13:24:11 +0200

    commit initial

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+      <nav>
+        <RouterLink to="/">Accueil</RouterLink>
+      </nav>
+  </header>
+
+  <RouterView />
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Accueil from '../views/Accueil.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Accueil
+    },
+  ]
+})
+
+export default router
