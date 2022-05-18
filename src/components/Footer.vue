a5ca3fb3fd4cffccee0f0f852fb68d2dbb9f16be
Aliciaiversenc <alicia.iversenc@edu.univ-fcomte.fr>
2022-05-18 14:06:15 +0200

    Ajout components

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-Secondaire"></div>
+  <a href="/src/views/Mentions.vue">Mentions légales</a>
+  <a href="/src/views/Social.vue">Soutenez nous</a>
+  <a href="">Contactez nous</a>
+  <div>Social</div>
+</template>
