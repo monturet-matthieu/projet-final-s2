5bbc28a52c1768175ba2b0aa895ac45aae2610ba
Aliciaiversenc <alicia.iversenc@edu.univ-fcomte.fr>
2022-05-20 10:07:13 +0200

    images page Arrivee1

---
+++ src/views/Arrivee1.vue
@@ -0,0 +1,65 @@
+<template >
+  <img src="/public/img/arrivee1.webp" alt="Robot explicatif" />
+
+  <h3>Complete ton profil</h3>
+  <h4>0/3 - Complété</h4>
+
+  <h2>CATEGORIES</h2>
+  <div class="flex flex-wrap">
+    <img
+      class="w-10"
+      src="/public/img/hygiene-inscription.webp"
+      alt="catégorie hygiene"
+    />
+    <img
+      class="w-10"
+      src="/public/img/bienetre-inscription.webp"
+      alt="catégorie bienetre"
+    />
+    <img
+      class="w-10"
+      src="/public/img/travail-inscription.webp"
+      alt="catégorie travail"
+    />
+    <img
+      class="w-10"
+      src="/public/img/sport-inscription.webp"
+      alt="catégorie sport"
+    />
+    <img
+      class="w-10"
+      src="/public/img/art-inscription.webp"
+      alt="catégorie art"
+    />
+    <img
+      class="w-10"
+      src="/public/img/cuisine-inscription.webp"
+      alt="catégorie cuisine"
+    />
+    <img
+      class="w-10"
+      src="/public/img/organisation-inscription.webp"
+      alt="catégorie organisation"
+    />
+    <img
+      class="w-10"
+      src="/public/img/entourage-inscription.webp"
+      alt="catégorie entourage"
+    />
+    <img
+      class="w-10"
+      src="/public/img/devperso-inscription.webp"
+      alt="catégorie devperso"
+    />
+    <img
+      class="w-10"
+      src="/public/img/economique-inscription.webp"
+      alt="catégorie economique"
+    />
+    <img
+      class="w-10"
+      src="/public/img/numerique-inscription.webp"
+      alt="catégorie numerique"
+    />
+  </div>
+</template>
