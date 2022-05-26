c223a00b41047948fd5403d89f0d35ad5ccdfc7f
Aliciaiversenc <alicia.iversenc@edu.univ-fcomte.fr>
2022-05-26 18:59:33 +0200

    Page Arrivee1 terminée

--- src/views/Arrivee1.vue
+++ src/views/Arrivee1.vue
@@ -1,65 +1,89 @@
-<template >
-  <img class="w-2/3" src="/public/img/arrivee1.webp" alt="Robot explicatif" />
+<template>
+  <div class="font-karla">
+    <img class="p-4" src="/public/img/arrivee1.webp" alt="Robot explicatif" />
 
-  <h3>Complete ton profil</h3>
-  <h4>0/3 - Complété</h4>
+    <h3 class="text-2xl font-bold text-center">Complete ton profil</h3>
+    <h4 class="text-xl font-bold text-Important text-center">0/3 - Complété</h4>
 
-  <h2>CATEGORIES</h2>
-  <div class="flex flex-wrap">
+    <h2 class="font-extrabold text-3xl font-gothic mt-10 mb-1 ml-8">
+      CATEGORIES
+    </h2>
+    <div class="flex flex-wrap">
+      <img
+        class="w-1/2"
+        src="/public/img/hygiene-inscription.webp"
+        alt="catégorie hygiene"
+      />
+      <img
+        class="w-1/2"
+        src="/public/img/bienetre-inscription.webp"
+        alt="catégorie bienetre"
+      />
+      <img
+        class="w-1/2"
+        src="/public/img/travail-inscription.webp"
+        alt="catégorie travail"
+      />
+      <img
+        class="w-1/2"
+        src="/public/img/sport-inscription.webp"
+        alt="catégorie sport"
+      />
+      <img
+        class="w-1/2"
+        src="/public/img/art-inscription.webp"
+        alt="catégorie art"
+      />
+      <img
+        class="w-1/2"
+        src="/public/img/cuisine-inscription.webp"
+        alt="catégorie cuisine"
+      />
+      <img
+        class="w-1/2"
+        src="/public/img/organisation-inscription.webp"
+        alt="catégorie organisation"
+      />
+      <img
+        class="w-1/2"
+        src="/public/img/entourage-inscription.webp"
+        alt="catégorie entourage"
+      />
+      <img
+        class="w-1/2"
+        src="/public/img/ecologique-inscription.webp"
+        alt="catégorie devperso"
+      />
+      <img
+        class="w-1/2"
+        src="/public/img/devperso-inscription.webp"
+        alt="catégorie devperso"
+      />
+      <img
+        class="w-1/2"
+        src="/public/img/economique-inscription.webp"
+        alt="catégorie economique"
+      />
+      <img
+        class="w-1/2"
+        src="/public/img/numerique-inscription.webp"
+        alt="catégorie numerique"
+      />
+    </div>
+    <div class="flex flex-row justify-center mt-6">
+      <img
+        class="w-14 mr-24"
+        src="/public/icon/precedent-orange.webp"
+        alt="precedent"
+      />
+      <a href="/src/views/Arrivee2.vue">
+        <img class="w-14 ml-24" src="/public/icon/next.webp" alt="suivant" />
+      </a>
+    </div>
     <img
-      class="w-1/2"
-      src="/public/img/hygiene-inscription.webp"
-      alt="catégorie hygiene"
-    />
-    <img
-      class="w-1/2"
-      src="/public/img/bienetre-inscription.webp"
-      alt="catégorie bienetre"
-    />
-    <img
-      class="w-1/2"
-      src="/public/img/travail-inscription.webp"
-      alt="catégorie travail"
-    />
-    <img
-      class="w-1/2"
-      src="/public/img/sport-inscription.webp"
-      alt="catégorie sport"
-    />
-    <img
-      class="w-1/2"
-      src="/public/img/art-inscription.webp"
-      alt="catégorie art"
-    />
-    <img
-      class="w-1/2"
-      src="/public/img/cuisine-inscription.webp"
-      alt="catégorie cuisine"
-    />
-    <img
-      class="w-1/2"
-      src="/public/img/organisation-inscription.webp"
-      alt="catégorie organisation"
-    />
-    <img
-      class="w-1/2"
-      src="/public/img/entourage-inscription.webp"
-      alt="catégorie entourage"
-    />
-    <img
-      class="w-1/2"
-      src="/public/img/devperso-inscription.webp"
-      alt="catégorie devperso"
-    />
-    <img
-      class="w-1/2"
-      src="/public/img/economique-inscription.webp"
-      alt="catégorie economique"
-    />
-    <img
-      class="w-1/2"
-      src="/public/img/numerique-inscription.webp"
-      alt="catégorie numerique"
+      class="p-20"
+      src="/public/img/arrivee1-repaire.webp"
+      alt="page arrive 1/3"
     />
   </div>
 </template>
