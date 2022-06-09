ba25757a6ba4e0e20b409db5b46de7e5e648a2fb
lucas-mrmco <lucas.moramarco@edu.univ-fcomte.fr>
2022-06-09 12:12:47 +0200

    liste avatar affichee v1 - sans fctn add to profil

--- src/views/Arrivee2.vue
+++ src/views/Arrivee2.vue
@@ -8,8 +8,89 @@
     <h2 class="font-extrabold text-3xl font-gothic mt-10 mb-1 ml-8">
       Complète ton profil
     </h2>
-    <h3>NOM</h3>
-    <h4>Avatar</h4>
+    <h3>Avatar</h3>
+    
+  <div class="font-karla p-5 space-y-10">
+    <div class="grid-cols-3 md:px-10  grid text-center  px-3   w-full justify-center">
+      <h4>Femme</h4>
+      <h4>Jeune</h4>
+      <h4>Homme</h4>
+    </div>
+    
+    <h4 class="grid-cols-0">Peau claire - cheveux blonds</h4>
+    <div class="grid-cols-3 md:px-10 px-3 grid     w-full justify-center">
+      <img src="/public/img/avatars/f-peau1-cheveux1.png" alt="">
+      <img src="/public/img/avatars/nb-peau1-cheveux1.png" alt="">
+      <img src="/public/img/avatars/h-peau1-cheveux1.png" alt="">
+    
+    </div>
+    
+    <h4>Peau claire - cheveux chatains</h4>
+    <div class="grid-cols-3 md:px-10 px-3 grid     w-full justify-center">
+       <img src="/public/img/avatars/f-peau1-cheveux2.png" alt="">
+    <img src="/public/img/avatars/nb-peau1-cheveux2.png" alt="">
+    <img src="/public/img/avatars/h-peau1-cheveux2.png" alt="">
+    </div>
+   
+    
+    <h4>Peau claire - cheveux bruns</h4>
+    <div class="grid-cols-3 md:px-10 px-3 grid     w-full justify-center">
+      <img src="/public/img/avatars/f-peau1-cheveux3.png" alt="">
+    <img src="/public/img/avatars/nb-peau1-cheveux3.png" alt="">
+    <img src="/public/img/avatars/h-peau1-cheveux3.png" alt="">
+    </div>
+    
+    
+    <h4>Peau mate - cheveux blond</h4>
+    <div class="grid-cols-3 md:px-10 px-3 grid     w-full justify-center">
+      <img src="/public/img/avatars/f-peau2-cheveux1.png" alt="">
+    <img src="/public/img/avatars/nb-peau2-cheveux1.png" alt="">
+    <img src="/public/img/avatars/h-peau2-cheveux1.png" alt="">
+    </div>
+    
+    
+    <h4>Peau mate - cheveux chatains</h4>
+    <div class="grid-cols-3 md:px-10 px-3 grid     w-full justify-center">
+      <img src="/public/img/avatars/f-peau2-cheveux2.png" alt="">
+    <img src="/public/img/avatars/nb-peau2-cheveux2.png" alt="">
+    <img src="/public/img/avatars/h-peau2-cheveux2.png" alt="">
+    </div>
+    
+   
+    <h4>Peau mate - cheveux bruns</h4>
+    <div class="grid-cols-3 md:px-10 px-3 grid     w-full justify-center">
+      <img src="/public/img/avatars/f-peau2-cheveux3.png" alt="">
+    <img src="/public/img/avatars/nb-peau2-cheveux3.png" alt="">
+    <img src="/public/img/avatars/h-peau2-cheveux3.png" alt="">
+    </div>
+    
+   
+    <h4>Peau foncée - cheveux blonds</h4>
+    <div class="grid-cols-3 md:px-10 px-3 grid     w-full justify-center">
+       <img src="/public/img/avatars/f-peau3-cheveux1.png" alt="">
+    <img src="/public/img/avatars/nb-peau3-cheveux1.png" alt="">
+    <img src="/public/img/avatars/h-peau3-cheveux1.png" alt="">
+    </div>
+   
+    
+    <h4>Peau foncée - cheveux chatains</h4>
+    <div class="grid-cols-3 md:px-10 px-3 grid     w-full justify-center">
+        <img src="/public/img/avatars/f-peau3-cheveux2.png" alt="">
+    <img src="/public/img/avatars/nb-peau3-cheveux2.png" alt="">
+    <img src="/public/img/avatars/h-peau3-cheveux2.png" alt="">
+    </div>
+  
+    
+    <h4>Peau foncée - cheveux bruns</h4>
+    <div class="grid-cols-3 md:px-10 px-3 grid     w-full justify-center">
+      <img src="/public/img/avatars/f-peau3-cheveux3.png" alt="">
+    <img src="/public/img/avatars/nb-peau3-cheveux3.png" alt="">
+    <img src="/public/img/avatars/h-peau3-cheveux3.png" alt="">
+    </div>
+    
+    
+
+  </div>
     <h4>Couleur de peau</h4>
     <h4>Cheveux</h4>
     <div class="flex flex-row justify-center mt-6">
